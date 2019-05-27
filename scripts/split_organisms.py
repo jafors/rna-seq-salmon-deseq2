34581c3b78ec9d19211c1ff06048526e42b5f705
jafors <jafors@github.com>
2019-05-27 16:41:06 +0200

    deleted special case for mulit-reference

--- scripts/split_organisms.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-table = pd.read_csv(snakemake.input[0],sep='\t')
-
-human = table[table.Name.str.contains("ENST")]
-mouse = table[table.Name.str.contains("ENSMUST")]
-
-human.to_csv(snakemake.output[0], index=False, sep = '\t')
-mouse.to_csv(snakemake.output[1], index=False, sep = '\t')
